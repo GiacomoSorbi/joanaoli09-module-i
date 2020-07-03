762363e778013bc2ae990248c5ea3fe7d0203016
Joana Leitão Oliveira <joanaleitaooliveira@MacBook-Air-de-Joana.local>
2020-07-03 16:25:43 +0100

    corrections

--- JavaScript/action_page.js
+++ JavaScript/action_page.js
@@ -1,19 +1,3 @@
-/*const toggleModal = () => {
-  document.querySelector(".modal").classList.toggle("modal--hidden");
-};
-
-document.querySelector("#show-modal").addEventListener("click", toggleModal);
-
-document.querySelector("#submit").addEventListener("submit", event => {
-  event.preventDefault();
-  toggleModal();
-});
-
-document
-  .querySelector(".modal_close-bar span")
-  .addEventListener("click", toggleModal);
-*/
-
 document.getElementById("show-modal").addEventListener("click", function() {
   document.querySelector(".modal").style.display = "flex";
 });
@@ -22,14 +6,14 @@ function closeMe() {
   document.querySelector(".modal").style.display = "none";
 }
 
-document.querySelector("#show-modal").addEventListener("submit", event => {
+document.querySelector("#submit").addEventListener("submit", event => {
   event.preventDefault();
-  toggleModal();
-  var formData = new FormData(document.querySelector("#show-modal"));
+  let formData = new FormData(document.querySelector("#submit"));
   console.log(
     "Name:" + formData.get("name"),
     "Email:" + formData.get("email"),
     "Subject:" + formData.get("subject"),
     "Message:" + formData.get("message")
   );
+  alert("Thank you for your message!");
 });
